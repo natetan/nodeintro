68acbd2fa25e3b822d2971cfc39950fa99dfb077
yulongtan <swifties@uw.edu>
2015-10-23 13:35:02 -0700

    changed the response string

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-	response.send("Running Node backend")
+	response.send("This is an example of running Node as a backend");
 });
 
 app.listen(3000, function() {
