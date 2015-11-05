a4943c31e64e744a72168437525d09d3e0a1948c
yulongtan <swifties@uw.edu>
2015-11-05 14:01:44 -0800

    changed quote style

--- admin.js
+++ admin.js
@@ -3,9 +3,9 @@ var express = require("express");
 var app = express(); // the main app
 var admin = express(); // the sub app
 
-admin.get('/', function (req, res) {
+admin.get("/", function (req, res) {
   console.log(admin.mountpath); // /admin
-  res.send('Admin Homepage');
+  res.send("Admin Homepage");
 })
 
-app.use('/admin', admin); // mount the sub app
+app.use("/admin", admin); // mount the sub app
