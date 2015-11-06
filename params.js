02eb9a1f8ed8696412ef96f1e9a9b6011fc8d9d8
Yulong Tan <swifties@uw.edu>
2015-11-06 14:25:26 -0800

    params for callbacks

---
+++ params.js
@@ -0,0 +1,14 @@
+app.param('user', function(req, res, next, id) {
+
+  // try to get the user details from the User model and attach it to the request object
+  User.find(id, function(err, user) {
+    if (err) {
+      next(err);
+    } else if (user) {
+      req.user = user;
+      next();
+    } else {
+      next(new Error('failed to load user'));
+    }
+  });
+});
